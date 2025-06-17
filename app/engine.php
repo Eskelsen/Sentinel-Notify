1a8a381b049dd68de11ba0de43d2909282b9ab8b
Eskelsen <dev@microframeworks.com>
2025-06-17 23:36:16 +0000

    Added final data to reminder and multiple values for times

--- app/engine.php
+++ app/engine.php
@@ -4,17 +4,37 @@
 
 function stareAtClock($build,$cond){
 	$start = true;
+	if (timeFinished($cond)) {
+		return false;
+	}
 	foreach ($cond as $time => $value) {
-		$start = ($start AND (date($time)==$value));
+		$start = ($start AND timeMatchVerify($time,$value));
 	}
 	if ($start) {
 		$file = APP . 'build/' . $build . '.php';
 		if (!is_file($file)) {
 			microlog("Arquivo da build $build inexistente");
-			return;
+			return false;
 		}
 		include $file;
 		microlog("Arquivo da build $build executado");
-		return;
 	}
+}
+
+function timeMatchVerify($time, $value){
+	if (!is_array($value)) {
+		return date($time)==$value;
+	}
+	return in_array(date($time),$value);
+}
+
+function timeFinished(&$in){
+	if (empty($in['final'])) {
+		return false;
+	}
+	if (time()>=strtotime($in['final']) + 86399) {
+		return true;
+	}
+	unset($in['final']);
+	return false;
 }
